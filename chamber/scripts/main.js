ba6701c2ca6f9e1e3b547ffcf410179868b1c8ed
kandl25 <war21039@byui.edu>
2024-10-07 14:59:36 -0600

    10/7

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,25 +1,27 @@
-const currentyear = document.querySelector("#currentyear");
-const lastModified = document.querySelector("#lastModified")
+const currentYearElement = document.querySelector("#currentyear");
+const lastModifiedElement = document.querySelector("#lastmodified");
 const darkmode1 = document.querySelector("#themetoggle-desk");
 const darkmode2 = document.querySelector("#themetoggle-mobile");
 const bodyTag = document.querySelector("body");
-const mainnav = document.querySelector('.navigation')
+const mainnav = document.querySelector('.navigation');
 const hambutton = document.querySelector('#menu');
 
 hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('show');
-    hambutton.classList.toggle('show');
+  mainnav.classList.toggle('open');
+  hambutton.classList.toggle('open');
 });
 
 const today = new Date();
-let currentYear = today.getFullYear();
+const currentYear = today.getFullYear();
 
-currentYear.innerHTML = currentYear;
-lastModified.innerHTML = document.lastModified;
+currentYearElement.innerHTML = currentYear;
+lastModifiedElement.innerHTML = document.lastModified;
+
+darkModeTriggered();
 
 darkmode1.addEventListener('click', darkModeTriggered);
 darkmode2.addEventListener('click', darkModeTriggered);
 
 function darkModeTriggered() {
-    bodyTag.classList.toggle("dark-mode");
+  bodyTag.classList.toggle("dark-mode");
 }
