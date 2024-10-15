3f2fd21dc81542fcbcb9328199bca0f4c01551c4
kandl25 <war21039@byui.edu>
2024-10-14 21:32:19 -0600

    im done

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -15,3 +15,32 @@ const currentYear = today.getFullYear();
 
 currentYearElement.innerHTML = currentYear;
 lastModifiedElement.innerHTML = document.lastModified;
+
+//spotlight
+/*const url = "https://kandl25.github.io/wdd231/chamber/data/members.json";
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    randomMembers(data);
+  })
+  .catch(error => console.error('Error fetching JSON:', error));
+
+function randomMembers(data) {
+  const randomThree = [];
+  while (randomThree.length < 3) {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    if (!randomThree.includes(data[randomIndex])) {
+      randomThree.push(data[randomIndex]);
+    }
+  }
+  randomThree.forEach(member => {
+    displayRandomThree(member);
+  });
+}
+
+function displayRandomThree(member) {
+  const container = document.querySelector('#spotlight');
+  const memberElement = document.createElement('div');
+  memberElement.textContent = JSON.stringify(member);
+  container.appendChild(memberElement);
+}*/
