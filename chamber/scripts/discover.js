2b6c469087bbbab7f3b9452b2d970da46c96caaf
kandl25 <war21039@byui.edu>
2024-10-24 11:43:23 -0600

    wk5

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,22 @@
+const discoverMessage = document.querySelector(".discover-greeting");
+
+window.addEventListener("load", function (event) {
+  console.log("Page loaded");
+  let lastVisit = localStorage.getItem("lastVisit");
+
+  console.log(lastVisit);
+
+  if (!lastVisit) {
+    discoverMessage.textContent =
+      "Welcome! Let us know if you have any questions";
+    localStorage.setItem("lastVisit", Date.now());
+  } else if (lastVisit && Date.now() - lastVisit < 86400000) {
+    discoverMessage.textContent = "Back so soon! Awesome!";
+  } else if (lastVisit && Date.now() - lastVisit > 86400000) {
+    let daysSinceLastVisit = Math.floor((Date.now() - lastVisit) / 86400000);
+    if (daysSinceLastVisit > 1) {
+      plural = "s";
+    }
+    discoverMessage.textContent = `You last visited ${daysSinceLastVisit} day${plural} ago.`;
+  }
+});
