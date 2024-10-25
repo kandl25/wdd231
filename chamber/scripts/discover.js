afcd6a637a665d91014b890095de4819bc3c8bb0
kandl25 <war21039@byui.edu>
2024-10-25 15:51:16 -0600

    trial

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,22 +1,17 @@
 const discoverMessage = document.querySelector(".discover-greeting");
 
 window.addEventListener("load", function (event) {
-  console.log("Page loaded");
-  let lastVisit = localStorage.getItem("lastVisit");
-
-  console.log(lastVisit);
-
-  if (!lastVisit) {
-    discoverMessage.textContent =
-      "Welcome! Let us know if you have any questions";
-    localStorage.setItem("lastVisit", Date.now());
-  } else if (lastVisit && Date.now() - lastVisit < 86400000) {
-    discoverMessage.textContent = "Welcome back!";
-  } else if (lastVisit && Date.now() - lastVisit > 86400000) {
-    let daysSinceLastVisit = Math.floor((Date.now() - lastVisit) / 86400000);
-    if (daysSinceLastVisit > 1) {
-      plural = "s";
+    console.log("Page loaded");
+    let lastVisit = localStorage.getItem("lastVisit");
+    console.log(lastVisit);
+    if (!lastVisit) {
+        discoverMessage.textContent = "Welcome! Let us know if you have any questions";
+        localStorage.setItem("lastVisit", Date.now());
+    } else if (lastVisit && Date.now() - Number(lastVisit) < 86400000) {
+        discoverMessage.textContent = "Welcome back!";
+    } else if (lastVisit && Date.now() - Number(lastVisit) > 86400000) {
+        let daysSinceLastVisit = Math.floor((Date.now() - Number(lastVisit)) / 86400000);
+        let plural = daysSinceLastVisit > 1 ? "s" : "";
+        discoverMessage.textContent = `You last visited ${daysSinceLastVisit} day${plural} ago.`;
     }
-    discoverMessage.textContent = `You last visited ${daysSinceLastVisit} day${plural} ago.`;
-  }
 });
