b0a0fad4143f2bdd8b00a5cf9520c26d383aaa36
kandl25 <war21039@byui.edu>
2024-10-26 20:04:03 -0600

    Lookin good!

--- project/scripts/join.js
+++ project/scripts/join.js
@@ -17,3 +17,48 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('form-body');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const parentFirstName = form.elements['first'].value;
+        const parentLastName = form.elements['last'].value;
+        const studentFirstName = form.elements['student-first'].value;
+        const studentLastName = form.elements['student-last'].value;
+        const email = form.elements['email'].value;
+        const phone = form.elements['phone'].value;
+        const age = form.elements['age'].value;
+        const birthday = form.elements['birthday'].value;
+        const danceClass = form.elements['class'].value;
+
+        const params = new URLSearchParams();
+        params.append('first', parentFirstName);
+        params.append('last', parentLastName);
+        params.append('student-first', studentFirstName);
+        params.append('student-last', studentLastName);
+        params.append('email', email);
+        params.append('phone', phone);
+        params.append('age', age);
+        params.append('birthday', birthday);
+        params.append('class', danceClass);
+
+        window.location.href = `thankyou.html?${params.toString()}`;
+    });
+});
+/* thank you form */
+document.addEventListener('DOMContentLoaded', function() {
+    const params = new URLSearchParams(window.location.search);
+    const details = `
+            <strong>Parent:</strong> ${params.get('first')} ${params.get('last')}<br>
+            <strong>Student:</strong> ${params.get('student-first')} ${params.get('student-last')}<br>
+            <strong>Email:</strong> ${params.get('email')}<br>
+            <strong>Phone:</strong> ${params.get('phone')}<br>
+            <strong>Age:</strong> ${params.get('age')}<br>
+            <strong>Birthday:</strong> ${params.get('birthday')}<br>
+            <strong>Class:</strong> ${params.get('class')}
+        `;
+    document.getElementById('details').innerHTML = details;
+});
