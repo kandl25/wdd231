0df7ff8e8c8c45583f8e61f1cc320f6c7c4bbad4
kandl25 <war21039@byui.edu>
2024-10-27 04:11:13 -0600

    done

--- project/scripts/classes.js
+++ project/scripts/classes.js
@@ -1,58 +1,55 @@
 const cardsContainer = document.querySelector("#schedule-container");
-const url = "https://github.com/kandl25/wdd231/blob/main/project/data/class.json";
+const url = "https://raw.githubusercontent.com/kandl25/wdd231/refs/heads/main/project/data/class.json";
 
 async function getClasses() {
-  const response = await fetch(url);
-  const data = await response.json();
-  displayClasses(data.class);
-}
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.log(classes)
+    displayClasses(data.classes);
+  }
+  
+    const displayClasses = (classes) => {
+        cardsContainer.innerHTML = "";
+  
+    classes.forEach((classInfo) => {
+        const card = document.createElement("div");
+        card.classList.add("directory");
+  
+        const style = document.createElement("h2");
+        style.classList.add("style");
+        style.textContent = classInfo.style;
+        card.appendChild(style);
+    
+        const image = document.createElement("img");
+        image.setAttribute("src", classInfo.image);
+        image.setAttribute("alt", `${classInfo.style} class image`);
+        image.setAttribute("loading", "lazy");
+        image.setAttribute("width", 310);
+        image.setAttribute("height", 500);
+        card.appendChild(image);
 
-const displayClasses = (classes) => {
-  cardsContainer.innerHTML = "";
-  let counter = 0;
-
-  classes.forEach(member => {
-    const card = document.createElement("div");
-    card.classList.add("directory");
-
-    const name = document.createElement("h2");
-    name.classList.add("style");
-    name.textContent = member.style;
-    card.appendChild(name);
-
-    const image = document.createElement("img");
-    image.setAttribute("src", member.image);
-    image.setAttribute("alt", member.name ? `${member.name} company logo` : "Company Logo");
-    if (counter > 3) {
-      image.setAttribute("loading", "lazy");
-    }
-    image.setAttribute("width", 300);
-    image.setAttribute("height", 500);
-    card.appendChild(image);
-
-    const address = document.createElement("p");
-    address.classList.add("teacher");
-    address.textContent = member.teacher;
-    card.appendChild(teacher);
-
-    const phone = document.createElement("p");
-    phone.classList.add("time");
-    phone.textContent = member.time;
-    card.appendChild(time);
-
-    const age = document.createElement("p");
-    url.classList.add("age");
-    url.textContent = member.age;
-    card.appendChild(age);
-
-    const cost = document.createElement("p");
-    url.classList.add("cost");
-    url.textContent = member.cost;
-    card.appendChild(cost);
- 
-    cardsContainer.appendChild(card);
-    counter++;
-  });
-}
-
-getClasses();
+        const teacher = document.createElement("p");
+        teacher.classList.add("teacher");
+        teacher.textContent = `Teacher: ${classInfo.teacher}`;
+        card.appendChild(teacher);
+    
+        const time = document.createElement("p");
+        time.classList.add("time");
+        time.textContent = `Time: ${classInfo.time}`;
+        card.appendChild(time);
+    
+        const age = document.createElement("p");
+        age.classList.add("age");
+        age.textContent = `Age: ${classInfo.age}`;
+        card.appendChild(age);
+    
+        const cost = document.createElement("p");
+        cost.classList.add("cost");
+        cost.textContent = `Cost: ${classInfo.cost}`;
+        card.appendChild(cost);
+    
+        cardsContainer.appendChild(card);
+    });
+  } 
+  
+  getClasses();
